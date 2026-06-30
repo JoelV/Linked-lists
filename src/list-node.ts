783e42e8283d4205b25bf8a2395f5197f76e94ad
Joel Villahermosa <joel.villahermosa@gmail.com>
2026-06-30 15:32:32 -0400

    implement ListNode class with constructor and properties

--- src/list-node.ts
+++ src/list-node.ts
@@ -1,1 +1,13 @@
-export class ListNode {}
+export class ListNode<T> {
+  public previous: ListNode<T> | null;
+  public next: ListNode<T> | null;
+
+  constructor(
+    public value: T,
+    previous?: ListNode<T> | null,
+    next?: ListNode<T> | null,
+  ) {
+    this.previous = previous ?? null;
+    this.next = next ?? null;
+  }
+}
--- src/list-node.unit.test.ts
+++ src/list-node.unit.test.ts
@@ -3,8 +3,24 @@ import { ListNode } from './list-node';
 describe('ListNode', () => {
   describe('constructor', () => {
     it('should create an instance', () => {
-      const listNode = new ListNode();
-      expect(listNode).toBeInstanceOf(ListNode);
+      const subject = new ListNode(1);
+      expect(subject).toBeInstanceOf(ListNode);
+    });
+    it('should set value to 1', () => {
+      const subject = new ListNode(1);
+      expect(subject.value).toEqual(1);
+    });
+    describe('when created with previous', () => {
+      it('should set value to previous', () => {
+        const subject = new ListNode(1, new ListNode(2));
+        expect(subject.previous?.value).toEqual(2);
+      });
+    });
+    describe('when created with next', () => {
+      it('should set value to previous', () => {
+        const subject = new ListNode(1, null, new ListNode(-1));
+        expect(subject.next?.value).toEqual(-1);
+      });
     });
   });
 });
