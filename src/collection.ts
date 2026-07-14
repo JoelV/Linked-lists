047328be1c4832cdb2195bcefc50f5988bad8995
Joel Villahermosa <joel.villahermosa@gmail.com>
2026-07-14 13:26:05 -0400

    feat: add Collection interface with generic methods for collection manipulation

---
+++ src/collection.ts
@@ -0,0 +1,21 @@
+export interface Collection<T> {
+  add?(element: T): boolean;
+  addAll?(collection: Collection<T> | Iterable<T>): boolean;
+  clear?(): void;
+  contains(value: unknown): boolean;
+  containsAll(collection: Collection<unknown> | Iterable<unknown>): boolean;
+  equals(other: unknown): boolean;
+  hashCode(): number;
+  isEmpty(): boolean;
+  iterator(): Iterator<T>;
+  parallelStream(): Iterable<T>;
+  remove?(value: T): boolean;
+  removeAll?(collection: Collection<T>): boolean;
+  removeIf(filter: (value: T) => boolean): boolean;
+  retainAll?(collection: Collection<T> | Iterable<unknown>): boolean;
+  size(): number;
+  spliterator(): IterableIterator<T>;
+  stream(): Iterable<T>;
+  toArray(): Array<T>;
+  toArray<U>(array: Array<U>): Array<U>;
+}
